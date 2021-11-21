b449d6e3f76ce27b5fe9d0f84bf4590a38f90e5d
Anwit Mondal <anwitmondal3299@gmail.com>
2021-11-22 02:02:38 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-let words={
-    "rainbow":10,
-    "flowers":15,
-    "doom":-5
-}
+const fs=require(`fs`);
+
+//It can also be done like this, but this reads the file like a text
+//document. It does not realize that it is a JSON
+// let words=fs.readFileSync('words.json','utf8');
+
+
+//This recognizes that it is a JSON
+let data = fs.readFileSync('words.json');
+let words=JSON.parse(data);
+
+
+
+console.log(words);
+
+
 
 const express=require(`express`)
 const app=express()
 const server=app.listen(3000)
+
 
 app.use(express.static(`public`))
 
@@ -29,7 +41,17 @@ app.get('/add/:word/:score?',(req,res)=>{
     if(!score){
         res.send(`Score is required`);
     }
+
     words[word]=Number(score);  
+    //JSON.stringify changes the JSON to a string 
+    //null and 2 is for indentation because stringify converts to a text with as little 
+    //characters or whitespaces as possible
+    let data=JSON.stringify(words,null,2);
+    fs.writeFile('words.json',data,(err)=>{
+        if(err) console.log(err);
+        console.log(`Successfully added new data`);
+    });
+
     res.send(words);
 })
 
@@ -51,6 +73,7 @@ app.get('/search/:word',(req,res)=>{
             "word" :word
         }
     }
+    
     
     res.send(reply);
 })
